2191e00e76c2417992c81e6640761f5dd842458e
Maksym Rechyn <maxr19777@gmail.com>
2025-05-25 11:49:14 +0200

    Added dz10 folder

---
+++ dz10/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CatImage from './CatImage';
+import styles from '../styles/App.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <CatImage />
+    </div>
+  );
+}
+
+export default App;
---
+++ dz10/src/components/CatImage.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styles from '../styles/CatImage.module.css';
+
+const CatImage = () => {
+  const [imageUrl, setImageUrl] = useState('');
+
+  const fetchCatImage = async () => {
+    try {
+      const response = await axios.get('https://api.thecatapi.com/v1/images/search');
+      setImageUrl(response.data[0].url);
+    } catch (error) {
+      console.error('Error fetching the cat image', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCatImage();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Random Cat Image</h1>
+      {imageUrl && <img src={imageUrl} alt="Random Cat" className={styles.image} />}
+      <button onClick={fetchCatImage} className={styles.button}>
+        Load New Image
+      </button>
+    </div>
+  );
+};
+
+export default CatImage;
